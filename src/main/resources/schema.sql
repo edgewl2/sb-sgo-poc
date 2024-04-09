e257f09ea568f1bc1140c14606ba50a2cdf48a96
Gerardo Hernandez <edgewl2@gmail.com>
2024-04-09 14:15:50 -0600

    feat(schema): nueva entidad de base de datos

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,13 @@ create table pais(
     primary key (id)
 );
 
+create table persona (
+    id bigint auto_increment not null,
+    nombre varchar(50),
+    apellido varchar(50),
+    pais_id varchar(2) not null,
+    primary key (id)
+);
+
+alter table persona add constraint persona_pais__fk foreign key (pais_id) references pais;
+
