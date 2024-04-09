aaa05034e049676ba95e016d419497eb8cf3bcc1
Gerardo Hernandez <edgewl2@gmail.com>
2024-04-09 14:05:17 -0600

    build(webpack): cambio para cargar webfont files

--- webpack.js
+++ webpack.js
@@ -7,57 +7,67 @@ module.exports = {
         jquery: 'jQuery',
     },
     entry: {
-    paises: './src/main/webapp/resources/src/paises/App.js',
+        paises: './src/main/webapp/resources/src/paises/App.js',
+        personas: './src/main/webapp/resources/src/personas/App.js',
     },
     output: {
         path: path.resolve(__dirname, 'src', 'main', 'resources', 'static'),
         filename: '[name]/app.bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        },
-        {
-            test: /\.(scss)$/,
-            use: [
-                {
-                    // Adds CSS to the DOM by injecting a `<style>` tag
-                    loader: 'style-loader'
-                },
-                {
-                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                    loader: 'css-loader'
-                },
-                {
-                    // Loader for webpack to process CSS with PostCSS
-                    loader: 'postcss-loader',
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        postcssOptions: {
-                            plugins() {
-                                return [
-                                    require('autoprefixer')
-                                ];
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'webfonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader',
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins() {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
                             }
                         }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
                     }
-                },
-                {
-                    // Loads a SASS/SCSS file and compiles it to CSS
-                    loader: 'sass-loader'
-                }
-            ]
-        },
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }]
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
     }
 
 };
