108729ed80d38e70bfab81e4867b704f66915370
Gerardo Hernandez <edgewl2@gmail.com>
2024-04-09 14:34:22 -0600

    feat(service): nuevo metodo para buscar un pais en DB

--- src/main/java/com/venturicg/poc/service/PaisesService.java
+++ src/main/java/com/venturicg/poc/service/PaisesService.java
@@ -9,4 +9,6 @@ public interface PaisesService {
 
     List<Pais> obtenerPaises();
 
+    Pais buscar(String id);
+
 }
--- src/main/java/com/venturicg/poc/service/impl/PaisServiceImpl.java
+++ src/main/java/com/venturicg/poc/service/impl/PaisServiceImpl.java
@@ -2,11 +2,14 @@ package com.venturicg.poc.service.impl;
 
 import com.venturicg.poc.repository.PaisDAO;
 import com.venturicg.poc.service.PaisesService;
+import com.venturicg.poc.service.exception.PaisOperacionException;
 import com.venturicg.poc.service.model.Pais;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class PaisServiceImpl implements PaisesService {
@@ -18,5 +21,12 @@ public class PaisServiceImpl implements PaisesService {
     @Override
     public List<Pais> obtenerPaises() {
         return paisDAO.obtenerPaises();
+    }
+
+    @Override
+    public Pais buscar(String id) {
+        Objects.requireNonNull(id, "El id no puede ser nulo o vacio.");
+        return paisDAO.buscar(id)
+                .orElseThrow(() -> new PaisOperacionException("No se encontró el país con el id " + id, HttpStatus.NOT_FOUND));
     }
 }
